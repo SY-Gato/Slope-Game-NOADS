3cd56db47dabb84d21d6a84231c73f8316393372
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-10-25 12:25:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     document.addEventListener('keydown' (e) => {
-        if (e.code == 71) {
+        if (e.key === "g") {
             alert("G?");
         }
     });
